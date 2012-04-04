dc09fd9f371fcfce59fff89e40c15de1f48c7b0d
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-04 16:19:18 -0400

    Correction of comments, 1st Function, & 2nd Function

--- birthdayHerbert.js
+++ birthdayHerbert.js
@@ -1,55 +1,90 @@
-//Start variables:                                                                               
-//Stating variables:                                                                             
+//Start Variables:                                                                               
+//Stating Variables:                                                                             
 //(String)Name of the boy                                                                        
 var boyName;
-boyName = "Herbert";
+//(String)Not met the age console.log 
+var rejectAge;
 //(Number)When the boy can buy a pet                                                             
 var ageStart;
 //(Array)Time with each animal                                                                   
 var timeSpent;
 
-//Variable values:                                                                               
+//Variable Values:                                                                               
 boyName = "Herbert";
 ageStart = 12;
 timeSpent = [10,15,20,30];
-
+rejectAge = boyName + " didn't know the store has a policy, to buy a pet the customer has to be " + ageStart + " years old.";
 
 //1st Function (Procedure)                                                                       
 var welcome = function() {
 //Argument Conditional                                                                           
-    if(boyName == "Herbert") {
+    if(boyName === "Herbert") {
 	console.log("A boy by the name of " + boyName + " wanted to buy a pet for his birthday.");
     } else {
-	alert("Wrong Name");
+	console.log("Wrong Name");
     }
 
-}
+};
+
+welcome();
+
 //2nd Function (Boolean)                                                                                             
-    var agePolicy = function(policy) {
+    var agePolicy = function() {
 	//Stating variables:                                                                                                 
         var boyBirthAge;//How old the boy turned                                                                     
         var ageOlder;//That the boy is older than 12                                                                 
-        var ageAllowed;
         var storedPolicy;
-//Display message of the age policy                                                                                  
-// console.log("The store owner has a policy, to buy a pet the customer has to be " + ageStart + " years old."); 
+
 //Variable Values:                                                                                                   
-        boyBirthAge = 15;
-        ageOlder = true;
-        truePolicy = "The store owner has a policy, to buy a pet the customer has to be " + ageStart + " years old.";
-        ageAllowed = "Once the owner saw that " + boyName + " was older than " + ageStart + " years old";
-        //      storedPolicy = policy;                                                                               
-//Display message of the age policy & Comparison                                                                     
-        if (policy === true ) {
-            //  console.log(truePolicy);                                                                             
-        } else {
-                console.log("The own said sorry " + boyName + " but you do not meet the age requirement of " + ageStart + " years old.");
-        }
-        return truePolicy;
+        truePolicy = boyName + " knew the store has a policy, to buy a pet the customer has to be " + ageStart + " years old.";
+	ageOlder = true;
+//Display message of the age policy & comparison                                                                     
+        if (ageOlder === true) {
+	    // console.log(truePolicy);                                                                             
+        } else {       
+	    console.log(rejectAge);
+	}
+	return truePolicy;
+    };
 
-    }
-welcome();
-agePolicy();                                                                                                       
-
-var ageCheck = agePolicy(true);
+agePolicy();
+var ageCheck = agePolicy();
 console.log(ageCheck);
+
+//3rd Function (Number)                                                                                          
+	var allowed = function() {
+//Stating Variables:
+	var ageAllowed;//Statement for console.log
+        var boyBirthAge;//How old the boy turned 
+        var ageOlder;//That the boy is older than 12
+	var mathAge;
+//Variable Values:
+//	ageAllowed = "Once the owner saw that " + boyName + " was older than " + ageStart + " years old";
+	boyBirthAge = 15;
+        ageOlder = true;
+	//	mathAge = boyBirthAge - ageStart;
+//While Loop
+        while (boyBirthAge > ageStart) {
+	    mathAge = boyBirthAge - ageStart;
+	    if (mathAge === 3) {
+      	    //ageStart++;
+	    ageAllowed = "Once the owner saw that " + boyName + " was older than " + ageStart + " years old he allowed" + boyName + " to look at the pets.";
+	    console.log(ageAllowed);  
+	    } else {
+		
+	    // while (!boyBirthAge > ageStart) {
+	console.log("The owner said sorry " + boyName + " but you do not meet the age requirement, you have " + mathAge + " years old.");
+	    }
+	    return mathAge;
+	}
+
+	};
+
+//Calling Functions
+//welcome();
+//agePolicy();     
+allowed();                                                                                             
+
+//var ageCheck = agePolicy();
+//console.log(ageCheck);
+	//	var checkAllowed = allowed(
