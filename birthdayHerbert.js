cd050a99012afd9bcb9856ecd6182bfeb9572b4e
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-04 11:10:30 -0400

    Changes and adding 2nd Function (Boolean)

--- birthdayHerbert.js
+++ birthdayHerbert.js
@@ -24,5 +24,32 @@ var welcome = function() {
     }
 
 }
+//2nd Function (Boolean)                                                                                             
+    var agePolicy = function(policy) {
+	//Stating variables:                                                                                                 
+        var boyBirthAge;//How old the boy turned                                                                     
+        var ageOlder;//That the boy is older than 12                                                                 
+        var ageAllowed;
+        var storedPolicy;
+//Display message of the age policy                                                                                  
+// console.log("The store owner has a policy, to buy a pet the customer has to be " + ageStart + " years old."); 
+//Variable Values:                                                                                                   
+        boyBirthAge = 15;
+        ageOlder = true;
+        truePolicy = "The store owner has a policy, to buy a pet the customer has to be " + ageStart + " years old.";
+        ageAllowed = "Once the owner saw that " + boyName + " was older than " + ageStart + " years old";
+        //      storedPolicy = policy;                                                                               
+//Display message of the age policy & Comparison                                                                     
+        if (policy === true ) {
+            //  console.log(truePolicy);                                                                             
+        } else {
+                console.log("The own said sorry " + boyName + " but you do not meet the age requirement of " + ageStart + " years old.");
+        }
+        return truePolicy;
 
+    }
 welcome();
+agePolicy();                                                                                                       
+
+var ageCheck = agePolicy(true);
+console.log(ageCheck);
